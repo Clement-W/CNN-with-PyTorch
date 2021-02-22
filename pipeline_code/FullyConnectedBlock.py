46c95905f2570e1071f0adfb7b9bf3ee2ab7a731
Wclement <clement@weinreich.fr>
2021-02-22 20:38:47 +0100

    Format python code with black

--- pipeline_code/FullyConnectedBlock.py
+++ pipeline_code/FullyConnectedBlock.py
@@ -1,12 +1,13 @@
 from torch import nn
 
+
 class FullyConnectedBlock(nn.Module):
-    def __init__(self,nbInput,nbClasses):
-        super(FullyConnectedBlock,self).__init__()
+    def __init__(self, nbInput, nbClasses):
+        super(FullyConnectedBlock, self).__init__()
         self.block = nn.Sequential(
-            nn.Linear(nbInput,nbClasses)
-            #nn.Dropout()
+            nn.Linear(nbInput, nbClasses)
+            # nn.Dropout()
         )
-    
+
     def forward(self, x):
         return self.block(x)
--- pipeline_code/Network.py
+++ pipeline_code/Network.py
@@ -2,17 +2,17 @@ from torch import nn
 from FullyConnectedBlock import FullyConnectedBlock
 from ConvBlock import ConvBlock
 
-class Network(nn.Module):
 
+class Network(nn.Module):
     def __init__(self, kernels, nb_classes=10):
-        super(Network,self).__init__()
-        layers=[]
-        layers.append(ConvBlock(1,kernels[0])) # 28*28*1 -> 14*14*16
-        layers.append(ConvBlock(kernels[0],kernels[1])) # 14*14*16 -> 7*7*32
-        layers.append(ConvBlock(kernels[1],kernels[2])) # 7*7*32 -> 3*3*64
-        layers.append(nn.Flatten()) # 576
-        layers.append(FullyConnectedBlock(3*3*kernels[-1],nb_classes))
- 
+        super(Network, self).__init__()
+        layers = []
+        layers.append(ConvBlock(1, kernels[0]))  # 28*28*1 -> 14*14*16
+        layers.append(ConvBlock(kernels[0], kernels[1]))  # 14*14*16 -> 7*7*32
+        layers.append(ConvBlock(kernels[1], kernels[2]))  # 7*7*32 -> 3*3*64
+        layers.append(nn.Flatten())  # 576
+        layers.append(FullyConnectedBlock(3 * 3 * kernels[-1], nb_classes))
+
         self.net = nn.Sequential(*layers)
 
     def forward(self, x):
